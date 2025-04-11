bfe444277162ef3a2f755ec667d77eb1d18fcf8d
Xinxiang Chen <chenxinxiang0207@hotmail.com>
2025-04-11 21:33:31 +0800

    transfer cloudsim version to 7.0.0

---
+++ modules/cloudsim-examples/src/main/java/scheduler/Main.java
@@ -0,0 +1,13 @@
+package scheduler;
+
+import scheduler.core.RRExample;
+import scheduler.core.PSOExample;
+
+public class Main {
+    public static void main(String[] args) {
+        PSOExample.run();
+//        RRExample.run();
+//        DynamicRLExample.run();
+    }
+
+}
